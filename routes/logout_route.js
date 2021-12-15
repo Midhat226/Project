bb6ae5109c9c7a6d0a6c38371aebc1f6a43b3dbc
Rutba Asghari <95971595+rutbaasghari20@users.noreply.github.com>
2021-12-15 18:27:23 +0500

    Delete logout_route.js

--- routes/logout_route.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-/* GET users listing. */
-router.get('/logout', function(req, res) {
-  req.session.destroy();
-  res.redirect('/login');
-});
-module.exports = router;
