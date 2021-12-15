7d5398fb58eb33481e1f3ad3fe1a37ce674360c7
Midhat226 <66376471+Midhat226@users.noreply.github.com>
2021-12-15 20:00:16 +0500

    Delete routes directory

--- routes/adminlogout_route.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-/* GET users listing. */
-router.get('/adminlogout', function(req, res) {
-  req.session.destroy();
-  res.redirect('/adminlogin');
-});
-module.exports = router;
