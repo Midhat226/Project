b2a9da10cb80ecb8dd0f9464768398c287132081
Midhat226 <66376471+Midhat226@users.noreply.github.com>
2021-12-15 19:56:46 +0500

    Update app.js

--- app.js
+++ app.js
@@ -23,22 +23,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 
-var registrationRouter = require('./routes/registration_route');
-var loginRouter = require('./routes/login_route');
-var dashboardRouter = require('./routes/dashboard_route');
-var logoutRouter = require('./routes/logout_route');
+var UserregistrationRouter = require('./routes/userregistration_route');
+var UserloginRouter = require('./routes/userlogin_route');
+var UserdashboardRouter = require('./routes/userdashboard_route');
+var UserlogoutRouter = require('./routes/userlogout_route');
 var contactusRouter = require('./routes/contactus_route');
 var adminloginRouter = require('./routes/adminlogin_route');
 var adminlogoutRouter = require('./routes/adminlogout_route');
 var admindashboardRouter = require('./routes/admindashboard_route');
-app.use('/', registrationRouter);
-app.use('/', loginRouter);
-app.use('/', dashboardRouter);
-app.use('/', logoutRouter);
+var bookrideRouter = require('./routes/book_ride_route');
+app.use('/', UserregistrationRouter);
+app.use('/', UserloginRouter);
+app.use('/', UserdashboardRouter);
+app.use('/', UserlogoutRouter);
 app.use('/', contactusRouter);
 app.use('/', adminloginRouter);
 app.use('/', adminlogoutRouter);
 app.use('/', admindashboardRouter);
+app.use('/', bookrideRouter);
 
 //setting up server
 app.listen(3000,()=>{
