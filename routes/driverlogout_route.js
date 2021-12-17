40c5da495ba39d82abd4a25459f235a1af0a36c4
Rutba Asghari <95971595+rutbaasghari20@users.noreply.github.com>
2021-12-18 01:28:17 +0500

    Delete routes directory

--- routes/driverlogout_route.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-/* GET drivers listing. */
-router.get('/driverlogout', function(req, res) {
-  req.session.destroy();
-  res.redirect('/driverlogin');
-});
-module.exports = router;
